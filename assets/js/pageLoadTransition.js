467b7e086254e43030be63b54c4b9be5dfe157ae
Tim Smith <tim.smith.hdg@gmail.com>
2019-11-11 15:43:49 -0800

    Body and Footer

--- assets/js/pageLoadTransition.js
+++ assets/js/pageLoadTransition.js
@@ -3,6 +3,8 @@ const menu = document.querySelector(".menu");
 const logo = document.querySelector(".logo");
 const logoSpans = [...document.querySelectorAll(".logo-span")];
 const social = document.querySelector(".social");
+const socialLinkText = [...document.querySelectorAll(".social-link-text")];
+const socialLinkIcons = [...document.querySelectorAll(".social-link-icons")];
 
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(function() {
@@ -13,11 +15,21 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 let scrollpos = window.scrollY;
+
 const add_class_on_scroll = () => {
   nav.classList.add("nav-fadeIn");
   logo.classList.add("logo-fadeIn");
   logoSpans.forEach(function(span) {
     span.classList.add("logo-span-fadeIn");
+  });
+  socialLinkText.forEach(function(text) {
+    if (text.classList.contains("show")) {
+      text.classList.remove("show");
+    }
+    text.classList.add("hide");
+  });
+  socialLinkIcons.forEach(function(icon) {
+    icon.classList.add("show");
   });
 };
 const remove_class_on_scroll = () => {
@@ -25,6 +37,14 @@ const remove_class_on_scroll = () => {
   logo.classList.remove("logo-fadeIn");
   logoSpans.forEach(function(span) {
     span.classList.remove("logo-span-fadeIn");
+  });
+  socialLinkText.forEach(function(text) {
+    text.classList.remove("hide");
+    text.classList.add("show");
+  });
+  socialLinkIcons.forEach(function(icon) {
+    icon.classList.remove("show");
+    icon.classList.add("hide");
   });
 };
 
